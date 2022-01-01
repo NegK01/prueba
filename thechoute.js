79095576f85722267c8f7d05f39b0320345052d5
NegK01 <92654015+NegK01@users.noreply.github.com>
2021-12-31 20:52:51 -0600

    Update thechoute.js

--- thechoute.js
+++ thechoute.js
@@ -1517,7 +1517,7 @@ case 'menu':
                 if (isBan) return reply (baby.only.benned)	
                 reply('Cargando Menu, Porfavor no hacer spam de mensaje')
                txtt =`${cmenu}`              
-                sendButLocation(from, `${txtt}`, `Creador @${Oowner.split("@")[0]}`, fs.readFileSync("./media/imagen/absu.jpg"),
+                sendButLocation(from, `${txtt}`, `Creador @${Oowner.split("@")[0]}`, fs.readFileSync("./media/imagen/neg.jpg"),
                 buttons =[ {buttonId:`ml`,buttonText:{displayText: 'Lista de Menus'},type:1},
                 {buttonId:`sex`,buttonText:{displayText:'Como Tener Este Bot'},type:1},
                 {buttonId:`grupo`,buttonText:{displayText:'Grupos OFC'},type:1}],
@@ -2901,7 +2901,7 @@ break
 case 'menuofc':
               if (!isRegister) return reply(baby.only.usrReg)                
               reply('Cargando menu, porfavor espera.\n\nNo hagas spam de comandos')
-              leo.sendMessage(from, fs.readFileSync('./media/imagen/bot-baby.jpg'), image, {caption: `${allmenu}` , quoted: chr, contextInfo: { mentionedJid: [sender,Oowner]}})
+              leo.sendMessage(from, fs.readFileSync('./media/imagen/negbot.jpg'), image, {caption: `${allmenu}` , quoted: chr, contextInfo: { mentionedJid: [sender,Oowner]}})
               break
 
 case 'menugrupo':
