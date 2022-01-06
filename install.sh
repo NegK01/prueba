8b9c8da85e1ed6ba084b7b3c67e8d99a2c0ec2aa
NegK01 <92654015+NegK01@users.noreply.github.com>
2022-01-06 16:24:24 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,37 @@ echo -e '\e[1;32m
 ░█▄─░█ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀█ █▀▀█ ─▀─ █▀▀█ █▀▀ 
 ░█░█░█ █▀▀ █── █▀▀ ▀▀█ █▄▄█ █▄▄▀ ▀█▀ █──█ ▀▀█ 
 ░█──▀█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀──▀ ▀─▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀'
-apt-get install ffmpeg libwebp -y
-apt-get install wget -y
-apt-get install imagemagick -y
-npm install
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install -g npm@6
+npm install toml
+npm i crypto-js
+npm i moment-timezone
+npm i axios
+npm i node-fetch@2.6.1 - npm
+npm i requests 
+npm install request --save
+npm i cfonts
+npm i spinnies
+npm i image-to-base64
+npm i remove.bg
+npm i fluent-ffmpeg
+npm i @adiwajshing/baileys
+npm i performance-now
+npm i form-data
+npm i yt-search
+npm i google-it
+npm i g-i-s
+npm i hxz-api
+npm i cheerio
 npm i qrcode
 
 echo -e '\e[1;32m
